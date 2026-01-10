7e6cf5d62e4a5f7deed4057acddb62738947660a
초우 <terbbal31@dgist.ac.kr>
2026-01-10 17:36:08 +0900

    tab2 사진 꽉 채우기 성공

--- app/src/main/java/com/example/mukeatlist/ui/photofeed/PhotoGridItem.kt
+++ app/src/main/java/com/example/mukeatlist/ui/photofeed/PhotoGridItem.kt
@@ -3,6 +3,9 @@ package com.example.mukeatlist.ui.photofeed
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -10,13 +13,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
-
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.CheckCircle
-import androidx.compose.material3.Icon
-import androidx.compose.ui.graphics.Color
 
 @Composable
 fun PhotoCell(
@@ -34,7 +34,12 @@ fun PhotoCell(
         AsyncImage(
             model = imageUrl,
             contentDescription = null,
-            modifier = Modifier.fillMaxSize()
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .fillMaxWidth()
+                .aspectRatio(1f)
+                .clip(RoundedCornerShape(4.dp))
+                .clickable(onClick=onClick)
         )
         
         if (isVisited) {
