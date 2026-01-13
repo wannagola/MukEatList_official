bd480583df9022823a16b84ab12d4802e0bd429d
Kim Changgyun <akeastshore@snu.ac.kr>
2026-01-13 21:55:37 +0900

    feat: tab3 badge page added

--- app/src/main/java/com/example/mukeatlist/viewmodel/BadgeUiState.kt
+++ app/src/main/java/com/example/mukeatlist/viewmodel/BadgeUiState.kt
@@ -1,6 +1,7 @@
 package com.example.mukeatlist.viewmodel
 
 data class BadgeUiState(
+    val id: Int,
     val categoryId: String,
     val title: String, // e.g. "쯔양 맛집 부수기"
     val description: String, // e.g. "쯔양 맛집 완전 정복 완료!"
